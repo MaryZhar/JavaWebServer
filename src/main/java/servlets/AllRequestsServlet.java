862495ebbb5ccafd7710370c148e1a1c8df5bc47
MaryZhar <61027703+MaryZhar@users.noreply.github.com>
2025-05-24 14:43:26 +0300

    First commit

---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import com.sun.corba.se.spi.ior.ObjectKey;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Mary
+ */
+public class AllRequestsServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        Map<String,Object> pageVariables = createPageVariablesMap(request);
+        pageVariables.put("message", "");
+        response.getWriter().println();
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request){
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("method", request.getMethod());
+        pageVariables.put("URL",request.getRequestURL().toString());
+        pageVariables.put("pathInfo",request.getPathInfo());
+        pageVariables.put("sessionId",request.getSession().getId());
+        pageVariables.put("parameters", request.getParameterMap().toString());
+        return pageVariables;
+    }
+
+}
