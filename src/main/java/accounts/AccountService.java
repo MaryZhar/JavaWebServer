634713dd976d9105573193e76437e31ae13f0fe7
MaryZhar <61027703+MaryZhar@users.noreply.github.com>
2025-05-28 23:54:44 +0300

    Update AccountService.java

--- src/main/java/accounts/AccountService.java
+++ src/main/java/accounts/AccountService.java
@@ -1,7 +1,19 @@
 package accounts;
 
+import org.omg.CORBA.UserException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Mary
  */
 public class AccountService {
+    private final Map<String, UserProfile> loginToProfile;
+    private final Map<String, UserProfile> sessionIdToProfile;
+
+    public AccountService(){
+        loginToProfile = new HashMap<>();
+        sessionIdToProfile = new HashMap<>();
+    }
 }
